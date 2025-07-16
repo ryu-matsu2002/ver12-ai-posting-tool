a86eca19d0bc6389142edfcb7b993a43adde8c46
ryu-matsu0624 <ryu.nosuke.iphone@gmail.com>
2025-07-16 19:25:31 +0900

    本番環境への修正を反映

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,13 @@
-# wsgi.py
+import logging
+
+# ← この設定を先頭に追加
+logging.basicConfig(
+    level=logging.DEBUG,  # ← WARNINGやDEBUGも出力する
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 # Flask アプリケーションを作成するために必要なインポート
 from app import create_app
 
 # アプリケーションのインスタンスを一度だけ生成
-# create_app() は Flask アプリケーションの設定や初期化を行います
 app = create_app()
-
-# WSGI サーバー（gunicornなど）でこのアプリケーションを実行する準備が整いました
